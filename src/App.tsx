da6a0746689a96dfa8fb5a9e3fcf619523ea6e1d
Atimscreative <timilehinsunday144@gmail.com>
2025-06-14 12:42:58 +0100

    Added new pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Homepage from "./page/Homepage";
 import NotFound from "./page/NotFound";
 import AppLayout from "./components/AppLayout";
+import About from "./page/About";
+import Utilities from "./page/Utilities";
+import AIPersonas from "./page/AIPersonas";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route element={<AppLayout />}>
             <Route index path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/utilities" element={<Utilities />} />
+            <Route path="/personas" element={<AIPersonas />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
---
+++ src/page/AIPersonas.tsx
@@ -0,0 +1,3 @@
+export default function AIPersonas() {
+  return <div></div>;
+}
